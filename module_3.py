c15d89ceee28ef91557773428d40dee9f04e69a1
GrishakV <vovan095@gmail.com>
2020-10-05 02:30:24 +0300

    Исправил log

--- module_3.py
+++ module_3.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     S = 0.0
 
     for k in range(1, n + 1):
-        a = math.log10(k * x) / (k * k)
+        a = math.log(k * x) / (k * k)
         S += a
 
     print(f"S = {S}")
